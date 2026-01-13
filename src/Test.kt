b829e22335cb0d85d146e46a19a8c96d2de467dd
vasy <zdan2007@gmail.com>
2026-01-13 16:04:11 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -1,3 +1,13 @@
+abstract class Human(val name: String){
+    abstract var age: Int
+    abstract fun hello()
+}
+class Person(name: String) : Human(name){
+    override  var age: Int = 1
+    override  fun hello(){
+        println("My name is $name")
+    }
+}
 data class Item(
     val id: Int,
     val name: String,
@@ -26,6 +36,27 @@ val sword = Item(1, "Sword", 1)
     println(betterSword.toString())
     val(id, name, quantity) = betterSword
     println("Id предмета: $id\nИмя: $name\nКоличество: $quantity\n")
+//    val denis:Human
+//    val pavel:Human("Pavel")
+//    val maksim: Human = Person("Maksim")
+//    denis.hello()
+//    maksim.hello()
+    val denis: Person = Person("Denis")
+    val maksim: Human = Person("Maksim")
+
 }
 
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+class Rectangle(val width: Float, val height: Float) : Figure(){
+    override fun perimeter(): Float {
+        return 2 * (width + height)
+    }
+
+    override fun area(): Float {
+        return width * height
+    }
+}
 
