e9e02eb2d298fb1929129c9ff3bed939247e8ef5
александра спиридонова <s9061722818@yandex.ru>
2026-02-18 19:48:00 +0300

    Create sealed model ModuleResult

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,50 @@
 import modules.EnergyGenerator
+import modules.ModuleResult
 import modules.ResearchLab
 import resources.OutpostResource
 import resources.ResourceManager
+
+fun handleModuleResult(result: ModuleResult) {
+    when (result) {
+        is ModuleResult.Success ->
+            println("Успех: ${result.message}")
+        is ModuleResult.ResourceProduced ->
+            println("Произведено: ${result.resourceName} +${result.amount}")
+        is ModuleResult.NotEnoughResources ->
+            println(
+                "Недостаточно ресурса ${result.resourceName}. " +
+                        "Нужно: ${result.required}, есть: ${result.available}"
+            )
+        is ModuleResult.Error ->
+            println("ОШИБКА: ${result.reason}")
+    }
+}
 
 fun main(){
     val manager = ResourceManager()
     manager.add(OutpostResource(1, "Minerals", 120))
     manager.add(OutpostResource(2, "Gas", 40))
+
     val generator = EnergyGenerator()
     val lab = ResearchLab()
     generator.performAction(manager)
     lab.performAction(manager)
     println()
     manager.printAll()
+
     val minerals = OutpostResource(1, "Minerals", 300)
     val gas = OutpostResource(2, "Gas", 100)
     manager.add(minerals)
     manager.add(gas)
     manager.printAll()
+
     val bonus = minerals.copy(amount = minerals.amount + 50)
     println("КопиЯ минералов с бонусом: $bonus")
 
+    val generatorResult = generator.performAction(manager)
+    val labResult = lab.performAction(manager)
+    handleModuleResult(generatorResult)
+    handleModuleResult(labResult)
+    println()
+    manager.printAll()
 }
--- src/modules/ResearchLab.kt
+++ src/modules/ResearchLab.kt
@@ -2,16 +2,17 @@ package modules
 
 import resources.ResourceManager
 
-class ResearchLab : OutpostModule(
-    "Исследовательская лаборатория"
-){
-    override fun performAction(manager: ResourceManager) {
+class ResearchLab : OutpostModule("Исследовательская лаборатория"){
+    override fun performAction(manager: ResourceManager): ModuleResult {
         val minerals = manager.get("Minerals")
         if (minerals == null || minerals.amount < 30){
-            println("Недостаточно минералов для исследования")
-            return
+            return ModuleResult.NotEnoughResources(
+                resourceName = "Minerals",
+                required = 30,
+                available = minerals?.amount ?:0
+            )
         }
         minerals.amount -= 30
-        println("Лаборатория проводит исследования (минералы -30)")
+        return ModuleResult.Success("Исследование завершино")
     }
 }
